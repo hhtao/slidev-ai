92f71aa2376641adae8ed22eb72230a4181f9426
li1553770945 <1553770945@qq.com>
2025-09-01 00:22:22 +0800

    feat:更新构建脚本

--- frontend/build-deploy.bat
+++ frontend/build-deploy.bat
@@ -1,3 +1,4 @@
-set VITE_API_BASE_URL=https://slidev.peacesheep.xyz/api
-set VITE_UPLOADS_BASE_URL=https://slidev.peacesheep.xyz/uploads
+set VITE_API_BASE_URL=https://slidev-ai.peacesheep.xyz/api
+set VITE_UPLOADS_BASE_URL=https://slidev-ai.peacesheep.xyz/uploads
+
 npm run build
---
+++ frontend/build-deploy.sh
@@ -0,0 +1,4 @@
+export VITE_API_BASE_URL=https://slidev-ai.peacesheep.xyz/api
+export VITE_UPLOADS_BASE_URL=https://slidev-ai.peacesheep.xyz/uploads
+
+npm run build
